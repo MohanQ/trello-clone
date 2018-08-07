f4225a0dab4c026d2ff8c1c196c46ee283c4d83b
Andras Molnar <andras.molnar@sapagroup.com>
2018-08-07 10:19:27 +0200

    Remove the mystery  ../../../Users/Molnar Andras/AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types from require method

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express = require('../../../Users/Molnar Andras/AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/express'),
+var express = require('express'),
     app = express(),
     port = process.env.PORT || 3200,
     mongoose = require('mongoose'),
     Card = require('./api/models/cardModel'),
     Column = require('./api/models/columnModel'),
     Board = require('./api/models/boardModel'),
-    bodyParser = require('../../../Users/Molnar Andras/AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/body-parser');
+    bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TrelloDb');
